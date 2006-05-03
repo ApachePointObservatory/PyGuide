1da541a72a26d9ff0e85ad602ce6b0ad68df1c71
Russell Owen <rowen@uw.edu>
2006-05-03 22:19:24 +0000

    Fixed the installation of ups data as per Craig's request. Made installation of ups data optional.

--- setup.py
+++ setup.py
@@ -8,32 +8,35 @@ import os
 if not hasattr(sys, 'version_info') or sys.version_info[0:2] < (2,2):
 	raise SystemExit("Python 2.2 or later required to build this module.")
 
-
 PkgName = "PyGuide"
 SrcDir = "src"
 PyDir = "python"
+UPSArg = "--ups"
 
-# create data file for the Fermi/Princeton UPS runtime loader
-sitePkgDir = distutils.sysconfig.get_python_lib(prefix="")
-if not os.path.exists("ups"):
-	os.mkdir("ups")
-upsfile = file("ups/PyGuide.table", "w")
-try:
-	upsfile.write("""File=Table
-Product=PyGuide
-Group:
-Flavor=ANY
-Common:
-Action=setup
-proddir()
-setupenv()
-pathAppend(PATH, ${UPS_PROD_DIR}/bin)
-pathAppend(PYTHONPATH, ${UPS_PROD_DIR}/%s)
-End:
-""" % (sitePkgDir,))
-finally:
-	upsfile.close()
-dataFiles = [["ups", ["ups/PyGuide.table"]]]
+dataFiles = []
+if UPSArg in sys.argv and "install" in sys.argv:
+	# create data file for the Fermi/Princeton UPS runtime loader
+	sys.argv.pop(sys.argv.index(UPSArg))
+	sitePkgDir = distutils.sysconfig.get_python_lib(prefix="")
+	if not os.path.exists("ups"):
+		os.mkdir("ups")
+	upsfile = file("ups/PyGuide.table", "w")
+	try:
+		upsfile.write("""File=Table
+	Product=PyGuide
+	Group:
+	Flavor=ANY
+	Common:
+	Action=setup
+	proddir()
+	setupenv()
+	pathAppend(PATH, ${UPS_PROD_DIR}/bin)
+	pathAppend(PYTHONPATH, ${UPS_PROD_DIR}/%s)
+	End:
+	""" % (sitePkgDir,))
+	finally:
+		upsfile.close()
+	dataFiles.append(["ups", ["ups/PyGuide.table"]])
 
 radProfExt = NumarrayExtension(
 	"radProf",
